437f027925d86600c3034419bf5c8028bffcda22
michael <NatkinMA@ya.ru>
2025-10-26 14:42:48 +0300

    Создание класса Settings для настроек приложения.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,26 +2,29 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
-        self.clock = pygame.time.Clock()        # Объект для отслеживания игрового времени.
-        self.screen = pygame.display.set_mode((1200, 800))
-        self.bg_color = (230, 230, 230)         # Задаем цвет, который будем использовать для фона.
+        self.clock = pygame.time.Clock()                # Объект для отслеживания игрового времени.
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """Запускает основной цикл игры."""
         while True:
-            for event in pygame.event.get():    # Отслеживание событий клавиатуры и мыши.
+            for event in pygame.event.get():            # Отслеживание событий клавиатуры и мыши.
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.bg_color)     # При каждом проходе цикла поверхность заливается выбранным цветом.
-            pygame.display.flip()               # Отображение последнего прорисованного экрана.
-            self.clock.tick(60)                 # Устанавливаем частоту кадров игры.
+            self.screen.fill(self.settings.bg_color)    # При каждом проходе поверхность заливается выбранным цветом.
+            pygame.display.flip()                       # Отображение последнего прорисованного экрана.
+            self.clock.tick(60)                         # Устанавливаем частоту кадров игры.
 
 
 if __name__ == "__main__":
