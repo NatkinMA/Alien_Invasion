a877b4522f741aeaa193c17e4a6fc052876202d0
michael <NatkinMA@ya.ru>
2025-10-26 14:33:26 +0300

    Создание окна Pygame и обработка ввода.

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,24 +5,23 @@ import pygame
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
-
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
-
+        self.clock = pygame.time.Clock()        # Объект для отслеживания игрового времени.
         self.screen = pygame.display.set_mode((1200, 800))
+        self.bg_color = (230, 230, 230)         # Задаем цвет, который будем использовать для фона.
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """Запускает основной цикл игры."""
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
+            for event in pygame.event.get():    # Отслеживание событий клавиатуры и мыши.
                 if event.type == pygame.QUIT:
                     sys.exit()
-
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+            self.screen.fill(self.bg_color)     # При каждом проходе цикла поверхность заливается выбранным цветом.
+            pygame.display.flip()               # Отображение последнего прорисованного экрана.
+            self.clock.tick(60)                 # Устанавливаем частоту кадров игры.
 
 
 if __name__ == "__main__":
