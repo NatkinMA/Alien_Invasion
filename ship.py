052435ab22778f2db3441efef4a466bda2ae77bc
michael <NatkinMA@ya.ru>
2025-10-26 15:46:38 +0300

    Добавление параметра для управления скоростью корабля.

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загружаем изображение корабля и получаем прямоугольник.
@@ -14,17 +15,22 @@ class Ship:
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Сохранение координаты центра корабля.
+        self.x = float(self.rect.x)
+
         # Флаг перемещения: начинается с неподвижного корабля.
         self.moving_right = False
         self.moving_left = False
 
-    def blitme(self):
-        """Рисует корабль в текущей позиции."""
-        self.screen.blit(self.image, self.rect)
-
     def update(self):
         """Обновление позиции корабля с учетом флага."""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Рисует корабль в текущей позиции."""
+        self.screen.blit(self.image, self.rect)
